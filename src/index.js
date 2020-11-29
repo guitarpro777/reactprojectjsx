7b1aa1baecadd43c0db253372a04fe1795973966
Luis Nunez <61037170+guitarpro777@users.noreply.github.com>
2020-11-28 22:55:25 -0700

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1>Favorite Foods</h1>
+    <ul>
+      <li>Chinese</li>
+      <li>Italian</li>
+      <li>Seafood</li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
